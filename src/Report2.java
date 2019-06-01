7b3b5e5209d664d2da48a96a1c2959450f97065d
Daniel Sosnowski <ds.sosnowski@gmail.com>
2019-06-01 13:39:36 +0200

    Imported objects from architecture

--- src/Report2.java
+++ src/Report2.java
@@ -1,7 +1,7 @@
 public class Report2 extends Report {
 
     @Override
-    public void Generate(){}
+    public void generate(){}
 
     public Report2(String Year){};
 }
--- src/Report3.java
+++ src/Report3.java
@@ -1,7 +1,8 @@
 public class Report3 extends Report {
 
     @Override
-    public void Generate(){}
+    public void generate(){}
 
-    public Report3(String Year){};
+
+    public Report3(String Name, String Surname, String Year){};
 }
