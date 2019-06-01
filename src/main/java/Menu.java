b12b2a5c77fb9f6bea3a94cb82d84fad30cbf6af
hpalonek <hpalonek@gmail.com>
2019-06-01 22:28:27 +0200

    dopieszczenie Menu, bo to nasza wizytowka

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -11,7 +11,8 @@ public class Menu {
     private int year;
     private String outputType;
 
-    public Menu() {}
+    public Menu() {
+    }
 
     public File getPath() {
         return path;
@@ -53,169 +54,178 @@ public class Menu {
         this.outputType = outputType;
     }
 
-    //String exit;
-
     String check;
+
+    public void displayReturnToMainMenuPanel() {
+        System.out.println("*** Chcesz wrócić do Menu głównego? Kliknij \"m\" ***");
+    }
 
     public void mainMenuPanel() {
 
-        System.out.println("Witaj w systemie generowania raportów");
+        System.out.println("-------Witaj w systemie generowania raportów-------");
         System.out.println();
-        System.out.println("Podaj ścieżkę do danych wejściowych.");
+        System.out.println("(A) Podaj ścieżkę do danych wejściowych. path = ");
+        System.out.println("*** Jeśli chcesz wyjśc z aplikacji wpisz \"q\" ***");
 
-        String pathString = scanner.next();
+        check = scanner.nextLine();
+
+        if (check.equals("q")) {
+            System.exit(0);
+        }
+
+        String pathString = check;
         this.path = new File(pathString);
 
-        // sprawdź czy ścieżka istnieje jak nie to daj wyjątek
+        if (this.path.exists()) {
+            reportTypePanel();
+        } else {
+            System.out.println("!!! Ścieżka nie istnieje, wpisz jeszcze raz. !!!");
+            mainMenuPanel();
+        }
 
-
-        reportTypePanel();
-
-
-        //System.out.println("Wpisz q aby zakonczyc");
-        //exit = scanner.next();
     }
 
     public void reportTypePanel() {
 
-        System.out.println("Wybierz rodzaj raportu:");
+        System.out.println("----------------------------------------------------");
+        System.out.println("(B) Wybierz rodzaj raportu:");
         System.out.println("1. Raport roczny wg. wszystkich pracowników");
         System.out.println("2. Raport roczny wg. projektów");
         System.out.println("3. Raport roczny pracownika");
         System.out.println("4. Raport roczny pracownika procentowy wg. projektu");
         System.out.println("5. Raport roczny top 10 zadań");
-        System.out.println("***chcesz wrócić do Menu głównego? Kliknij m");
+
+        displayReturnToMainMenuPanel();
 
         String check;
-        check = scanner.next();
+        check = scanner.nextLine();
 
         if (check.equals("m")) {
             mainMenuPanel();
         }
-        else{
-            this.reportType = Integer.parseInt(check);
-        }
 
-        if (this.reportType == 1 || this.reportType == 2 || this.reportType == 3 || this.reportType == 4 || this.reportType == 5 ) {
+        if (check.equals("1") || check.equals("2") || check.equals("3") || check.equals("4") || check.equals("5")) {
+            this.reportType = Integer.parseInt(check);
             dataPanel(this.reportType);
-        }
-        else {
-            System.out.println("Nie ma takiego raportu spróbuj jeszcze raz");
+        } else {
+            System.out.println("!!! Nie ma takiego raportu spróbuj jeszcze raz !!!");
             reportTypePanel();
         }
-
-
-
-        //System.out.println("Wpisz q aby zakonczyc");
-        //exit = scanner.next();
-
     }
+
     public void dataPanel(int type) {
+
+        System.out.println("----------------------------------------------------");
+        System.out.println("(C) Podaj dane potrzebne do raportu:");
+
+        displayReturnToMainMenuPanel();
 
         switch (type) {
             case 1:
-                    System.out.println("Podaj rok");
-                    this.year = Integer.parseInt(scanner.next());
-                    break;
+                System.out.println("Podaj rok: ");
 
-                    case 2:
-                        System.out.println("Raport niedostepny, wybierz inny");
-                    reportTypePanel();
-                    //System.out.println("Podaj rok");
-                    //this.year = Integer.parseInt(scanner.next());
+                try {
+                    this.year = Integer.parseInt(scanner.nextLine());
+                    outputTypePanel();
+                } catch (Exception error) {
+                    System.out.println("!!! Podany rok ma zły format !!!");
+                    dataPanel(type);
+                }
+                break;
+            case 2:
+                System.out.println("!!! Raport niedostepny, wybierz inny !!!");
+                reportTypePanel();
 
-                    //System.out.println("Wpisz q aby zakonczyc");
-                    //exit = scanner.next();
-                    break;
+                //System.out.println("Podaj rok");
+                //this.year = Integer.parseInt(scanner.next());
 
-                    case 3:
-                    System.out.println("Podaj imie pracownika");
-                    String firstName = scanner.next();
+                break;
+            case 3:
+                System.out.println("Podaj imie pracownika: ");
+                String firstName = scanner.nextLine();
 
-                    System.out.println("Podaj nazwisko pracownika");
-                    String lastName = scanner.next();
+                System.out.println("Podaj nazwisko pracownika ");
+                String lastName = scanner.nextLine();
 
-                    this.name = lastName + "_" + firstName;
+                this.name = lastName + "_" + firstName;
 
-                    System.out.println("Podaj rok");
-                    this.year = scanner.nextInt();
-                    break;
-                case 4:
-                    System.out.println("Raport niedostępny, wybierz inny");
-                    reportTypePanel();
+                System.out.println("Podaj rok: ");
 
-//                System.out.println("Podaj imie pracownika");
-//                String firstName = scanner.next();
+                try {
+                    this.year = Integer.parseInt(scanner.nextLine());
+                    outputTypePanel();
+                } catch (Exception error) {
+                    System.out.println("!!! Podany rok ma zły format !!!");
+                    dataPanel(type);
+                }
+
+                break;
+            case 4:
+                System.out.println("!!! Raport niedostępny, wybierz inny !!!");
+                reportTypePanel();
+
+//              System.out.println("Podaj imie pracownika");
+//              String firstName = scanner.next();
 //
-//                System.out.println("Podaj nazwisko pracownika");
-//                String lastName = scanner.next();
+//              System.out.println("Podaj nazwisko pracownika");
+//              String lastName = scanner.next();
 //
-//                this.name = lastName+"_"+firstName;
+//              this.name = lastName+"_"+firstName;
 //
-//                System.out.println("Podaj rok");
-//                this.year = scanner.nextInt();
+//              System.out.println("Podaj rok");
+//              this.year = scanner.nextInt();
 
-                    break;
-                case 5:
-                    System.out.println("Raport niedostępny, wybierz inny");
-                    reportTypePanel();
+                break;
+            case 5:
+                System.out.println("!!! Raport niedostępny, wybierz inny !!!");
+                reportTypePanel();
 
-                    // jakies podawanie słow  kluczowych itp
-                    // trzeba tez uzupełnić pola klasy!
+                // jakies podawanie słow  kluczowych itp
+                // trzeba tez uzupełnić pola klasy!
 
-                    break;
-                default:
-                    break;
+                break;
+            default:
+                break;
 
         }
-
-            System.out.println("***czy iść dalej? Kliknij d");
-            System.out.println("***chcesz wrócić do Menu głównego? Kliknij m");
-
-            check = scanner.next();
-
-            if (check.equals("d")) {
-                outputTypePanel();
-            }
-            if (check.equals("m")) {
-                mainMenuPanel();
-            }
     }
 
     public void outputTypePanel() {
 
-            System.out.println("Podaj rodzaj wyjścia:");
-            System.out.println("K - Konsola");
-            System.out.println("E - Excel");
-            System.out.println("P - Pdf");
-            System.out.println("W - Excel z wykresem");
-            System.out.println("Chcesz wrócić do menu głównego kliknij m");
+        System.out.println("----------------------------------------------------");
+        System.out.println("(D) Podaj rodzaj wyjścia:");
+        System.out.println("K - Konsola");
+        System.out.println("E - Excel");
+        System.out.println("P - Pdf");
+        System.out.println("W - Excel z wykresem");
 
-            this.outputType = scanner.next();
+        displayReturnToMainMenuPanel();
 
-            switch(this.outputType){
-                case "K":
-                    System.out.println("Dane przyjęte, raport zostanie wygenerowany");
-                    break;
-                case "E":
-                    System.out.println("Dane przyjęte, raport zostanie wygenerowany");
-                    break;
-                case "P":
-                    System.out.println("Wybacz funkcja niedostepna, spróbuj jeszcze raz");
-                    outputTypePanel();
-                    break;
-                case"W":
-                    System.out.println("Wybacz funkcja niedostepna, spróbuj jeszcze raz");
-                    outputTypePanel();
-                    break;
-                case"m":
-                    mainMenuPanel();
-                    break;
-                default:
-                    System.out.println("Nie ma takiego rodzaju, spróbuj jeszcze raz");
-                    outputTypePanel();
-                    break;
-            }
+        this.outputType = scanner.nextLine();
+
+        switch (this.outputType) {
+            case "K":
+                System.out.println("Dane przyjęte, raport zostanie wygenerowany");
+                break;
+            case "E":
+                System.out.println("Dane przyjęte, raport zostanie wygenerowany");
+                break;
+            case "P":
+                System.out.println("!!! Wybacz, funkcja niedostepna, spróbuj jeszcze raz !!!");
+                outputTypePanel();
+                break;
+            case "W":
+                System.out.println("!!! Wybacz, funkcja niedostepna, spróbuj jeszcze raz !!!");
+                outputTypePanel();
+                break;
+            case "m":
+                mainMenuPanel();
+                break;
+            default:
+                System.out.println("!!! Nie ma takiego rodzaju, spróbuj jeszcze raz !!!");
+                outputTypePanel();
+                break;
+        }
 
 
     }
