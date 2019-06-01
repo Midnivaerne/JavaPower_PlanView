f04e53bf719cfb864d637780d04faabdeb28ae34
Bartlomiej Bartczak <bartlomiej.bartczak@gmail.com>
2019-06-01 14:43:12 +0200

    Dodanie wstepnej architektury do App

--- src/App.java
+++ src/App.java
@@ -1,6 +1,35 @@
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        List<Path> xlsFilePath;
+        List<List<String>> rawData;
+
+        /*todo: wywołanie fukncji odpowiedzialnej za menu uzytkownika
+                obiekt menu zawiera informacje o roku, imieniu, nazwisku i rodzaju raportu
+                powyzsze atrybuty dostepne sa za pomoca getterow z obiektu menu
+        */
+
+
+        /*todo: file scanner zbiera i zapisuje do listy sciezki do plikow xls -> zapisuje je do xlsFilePath
+         */
+
+        //todo: for each dla xlsFilePath, w kazdej petli wywolujemy Reader (który za posrednictwem excellHandlera odczyta plik)
+        //      Reader przyjmuje jako argumenty sciezke do pliku xls, oraz utworzony model
+        //      Po wczytaniu i przetworzeniu danych z excellHandlera dodaje je do otrzymanego modelu,
+        //      Na koncu zwracany jest model z wstrzykniętymi nowymi danymi
+
+        //todo: tworzymy obiekt raport (switch case na bazie menu.getRaport()), jako konstruktor raportu podajemy model
+        //      Raport zwraca Listę list List<List<String>> generatedTable
+
+        //todo: Wywolanie obiektu metody print(generatedTable) z obiektu Printer,
+        //
     }
+
 }
