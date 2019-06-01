75e452f365bb9483883320bc046c5ec8afae70f2
fbiernat <filipbiernat@gmail.com>
2019-06-01 14:34:37 +0200

    Add Apache POI dependency

--- src/ExcelHandler.java
+++ src/ExcelHandler.java
@@ -9,6 +9,7 @@ public class ExcelHandler {
     //read form file
     //write to file
     public void write(String filename, Report report) {
-
+        // zaladowac (sparsowac) excele do modelu
+        // path, model
     }
 }
