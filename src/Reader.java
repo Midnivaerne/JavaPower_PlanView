6e0c0187285f9826066cb110ea2d7264a81bb069
wojtbauer <wojciech.brk1@gmail.com>
2019-06-02 10:04:57 +0200

    Reader done

--- src/Reader.java
+++ src/Reader.java
@@ -1,37 +1,50 @@
-import model.Model;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Reader {
+	
+	FilenameFilter filter;
+	
+//	
+//	public static void main(String[] args) {
+//	File folder = new File("C:\\");
+//	Reader reader = new Reader();
+//	
+//	
+//	reader.getExcels(folder);
+//
+//	}
+	
+	public Reader() {
+	filter = new FilenameFilter() {
+        public boolean accept(File directory, String fileName) {
+            return fileName.endsWith(".xls");
+        }
+      };
+	}
+	
+	public List<File> getExcels(File dataFolder){
+		List<File> result = new ArrayList<>();
 
-    private static Reader ourInstance = new Reader();
+		File[] listOfFilesWithExt = dataFolder.listFiles(filter);
+		File[] listOfFiles = dataFolder.listFiles();
+		
+		for (File file : listOfFilesWithExt ) {
+			result.add(file);
+			System.out.println(file.getName());
+		}
+		
+		for (File file : listOfFiles) {
+			if (file.isDirectory()) {
+				System.out.println(file);
+				this.getExcels(file);
+			}
+		}
+		
+		return result;
 
-    public static Reader getInstance() {
-        return ourInstance;
-    }
-
-    public Model parseData(String filename) throws FileNotFoundException {
-        if (filename == null)
-            return null;
-
-        Model model = new Model();
-        File file = new File(filename);
-        if (!file.exists())
-            throw new FileNotFoundException();
-
-        XSSFSheet workbook;
-        // filesystem crawler,
-        // rok
-
-        // miesiac
-
-        // excel, pracownicy
-        // ExcelHandler ?
-        return model;
-    }
-
-    private Reader() {
-    }
+		}
 }
