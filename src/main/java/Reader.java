0148b08702fd4bb3b8febc8c2dad1598cb31f04e
Bartlomiej Bartczak <bartlomiej.bartczak@gmail.com>
2019-06-02 10:40:52 +0200

    Scalenie brancha master z readerem

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -1,30 +1,12 @@
-<<<<<<< HEAD:src/Reader.java
-=======
-import model.Model;
-//import org.apache.poi.xssf.usermodel.XSSFSheet;
-
->>>>>>> 4ece2d295393714161f4ca7c95a23e0ba8e504b6:src/main/java/Reader.java
 import java.io.File;
 import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.List;
 
-
-<<<<<<< HEAD:src/Reader.java
 public class Reader {
 	
 	FilenameFilter filter;
-	
-//	
-//	public static void main(String[] args) {
-//	File folder = new File("C:\\");
-//	Reader reader = new Reader();
-//	
-//	
-//	reader.getExcels(folder);
-//
-//	}
-	
+
 	public Reader() {
 	filter = new FilenameFilter() {
         public boolean accept(File directory, String fileName) {
@@ -54,24 +36,4 @@ public class Reader {
 		return result;
 
 		}
-=======
-        Model model = new Model();
-        File file = new File(filename);
-        if (!file.exists())
-            throw new FileNotFoundException();
-
-        //XSSFSheet workbook;
-        // filesystem crawler,
-        // rok
-
-        // miesiac
-
-        // excel, pracownicy
-        // ExcelHandler ?
-        return model;
-    }
-
-    private Reader() {
-    }
->>>>>>> 4ece2d295393714161f4ca7c95a23e0ba8e504b6:src/main/java/Reader.java
 }
